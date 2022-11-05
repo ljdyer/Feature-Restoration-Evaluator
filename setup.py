2e7d06f207d3f8cfd8ba0f900c2c2e2d46605bc6
Laurence Dyer <ljdyer@gmail.com>
2022-11-05 21:34:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,6 @@ REQUIREMENTS = [
     'Jinja2',
 ]
 
-print("FRMG setup.")
-print(find_packages('src'))
-
 setup(
     name='frmg',
     version='1.0',
@@ -21,6 +18,6 @@ setup(
     author_email='ljdyer@gmail.com',
     url='https://github.com/ljdyer/Feature-Restorer-Metric-Getter',
     package_dir={'': 'src'},
-    packages=find_packages('src.feature_restorer_metric_getter'),
+    packages=['src.feature_restorer_metric_getter'],
     install_requires=REQUIREMENTS
 )

1273e7e0b52051209f9980cd1fe6ea0c3c8495c5
Laurence Dyer <ljdyer@gmail.com>
2022-11-05 19:20:04 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='Feature Restorer Metric Getter',
+    version='1.0',
+    description='',
+    author='Laurence Dyer',
+    author_email='ljdyer@gmail.com',
+    url='https://github.com/ljdyer/Feature-Restorer-Metric-Getter',
+    packages=['frmg'],
+)

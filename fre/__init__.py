406f1b1124990e1991cc0c2bc8f02b3e888c4560
Laurence Dyer <ljdyer@gmail.com>
2022-11-06 07:50:19 +0000

    Update __init__.py

--- fre/__init__.py
+++ fre/__init__.py
@@ -1,7 +1,2 @@
-# __all__ = ['feature_restoration_evaluator']
-
 from fre.feature_restoration_evaluator import \
     FeatureRestorationEvaluator  # noqa F401
-
-print('This is init, innit.')
-print(FeatureRestorationEvaluator)
